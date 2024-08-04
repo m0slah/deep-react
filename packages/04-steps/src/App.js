e74452dad99e895e58662a3bff9fddfa5f749746
m0slah <muhammedslahqadir@gmail.com>
2024-08-04 18:25:43 +0300

    add another state

--- packages/04-steps/src/App.js
+++ packages/04-steps/src/App.js
@@ -9,7 +9,8 @@ const messages = [
 
 function App() {
   const [step, setSteps] = useState(1);
-  const [user, setUser] = useState({ name: "john" });
+  // const [user, setUser] = useState({ name: "john" });
+  const [isOpen, setIsOpen] = useState(true);
 
   function handlePervious() {
     step > 1 && setSteps(step - 1);
@@ -20,37 +21,49 @@ function App() {
     //BAD PRACTICE
     // user.name = "test1";
 
-    setUser({ name: "terry" });
+    // setUser({ name: "terry" });
+  }
+
+  function handleIsOpen() {
+    setIsOpen(!isOpen);
   }
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-      </div>
+    <>
+      <button className="close" onClick={handleIsOpen}>
+        &times;
+      </button>
 
-      <p className="message">
-        step:{step} {messages[step - 1]}
-        <div>{user.name}</div>
-      </p>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
 
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handlePervious}
-        >
-          Prevoius
-        </button>
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handleNext}
-        >
-          Next
-        </button>
-      </div>
-    </div>
+          <p className="message">
+            step:{step} {messages[step - 1]}
+            {/* <div>{user.name}</div> */}
+          </p>
+
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePervious}
+            >
+              Prevoius
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
